311b8246d91179151593b26e0d1320a680f08ae5
Yusuf <yusufborucu@lastikcim.com.tr>
2024-09-07 20:36:38 +0300

    Initial project setup

---
+++ src/main/java/com/yusufborucu/todo_list_api/TodoListApiApplication.java
@@ -0,0 +1,13 @@
+package com.yusufborucu.todo_list_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yusufborucu/todo_list_api/TodoListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yusufborucu.todo_list_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
